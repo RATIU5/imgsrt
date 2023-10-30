f756ff1c38b734c8cdeb470365e3359558425e39
RATIU5 <jcmemmott20@outlook.com>
2023-10-30 14:10:10 -0700

    cheeky way of skipping executable

--- cmd/sort.go
+++ cmd/sort.go
@@ -154,8 +154,10 @@ func processFile(path string, destDir string) error {
 	if err != nil {
 		return err
 	}
-	destPath := filepath.Join(destDir, filepath.Base(path))
-	err = os.Rename(path, destPath) // use io.Copy if you want to copy instead of move
+	if filepath.Base(path) != "imgsrt.exe" {
+		destPath := filepath.Join(destDir, filepath.Base(path))
+		err = os.Rename(path, destPath) // use io.Copy if you want to copy instead of move
+	}
 	return err
 }
 
